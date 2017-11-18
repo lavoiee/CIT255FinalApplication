a91f5e5fa04de602bc7cc68dddb61e8da3e54ba6
lavoiee <ericjohnlavoie@gmail.com>
2017-11-18 05:10:06 -0500

    Added more pages

--- CIT255_UWP_Final_Project/ExchangesPage.xaml.cs
+++ CIT255_UWP_Final_Project/ExchangesPage.xaml.cs
@@ -13,20 +13,19 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CIT255_UWP_Final_Project
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ExchangesPage : Page
     {
-        public MainPage()
+        public ExchangesPage()
         {
             this.InitializeComponent();
         }
-
         private void Exchanges_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(ExchangesPage));
